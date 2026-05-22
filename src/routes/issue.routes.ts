e185479236cb703ae1784bf86f85728b03bbaa8c
Arvind Kumar <pratik.technorizen@gmail.com>
2026-05-22 17:58:06 +0530

    pending books route

--- src/routes/issue.routes.ts
+++ src/routes/issue.routes.ts
@@ -6,6 +6,6 @@ const router = Router();
 router.post("/issueBook", issueBook);
 router.get("/getStudentsWithBooks", getStudentsWithBooks);
 router.put("/return", returnBook);
-router.put("/pendingBooks", pendingBooks);
+router.get("/pendingBooks", pendingBooks);
 
 export default router;
